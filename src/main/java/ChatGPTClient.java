122a210ec52a5a62fd507b35f4408874bca7cb9b
Bhushan Kanhere <bhushan@Bhushans-MacBook-Pro.local>
2023-04-09 16:30:55 -0400

    Added file to store apiKey

--- src/main/java/ChatGPTClient.java
+++ src/main/java/ChatGPTClient.java
@@ -4,12 +4,23 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.service.OpenAiService;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ChatGPTClient {
 
     public static String getChatGPTResponse(String prompt) throws IOException {
-        String apiKey = "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";
+        //String apiKey = "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";
+        String apiKey = null;
+        BufferedReader reader = new BufferedReader(new FileReader("apiKey.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            apiKey = line;
+        }
+        if(apiKey == null) {
+            throw new RuntimeException("Set Your Api Key in apiKey.txt file");
+        }
 
         String model = "text-davinci-002";
         OpenAiService service = new OpenAiService(apiKey);
